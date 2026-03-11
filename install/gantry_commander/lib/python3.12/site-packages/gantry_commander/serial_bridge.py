ed58d359686393f3b7d5246b3193edccae510d6a
kha <khasmn>
2026-03-12 02:31:40 +0700

    first commit

--- install/gantry_commander/lib/python3.12/site-packages/gantry_commander/serial_bridge.py
+++ install/gantry_commander/lib/python3.12/site-packages/gantry_commander/serial_bridge.py
@@ -13,7 +13,8 @@ class SerialBridge(Node):
         
         try:
             # Arduino port
-            self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=0.1)
+            self.ser = serial.Serial('/dev/arduino_gantry', 115200, timeout=0.1)
+            self.get_logger().info("Serial connection established.")
             time.sleep(2.0)
             threading.Thread(target=self.read_serial, daemon=True).start()
             threading.Thread(target=self.user_interface, daemon=True).start()
@@ -64,14 +65,23 @@ class SerialBridge(Node):
     def listener_callback(self, msg):
         if self.is_ready and self.streaming_unlocked:
             try:
-                x = msg.position[msg.name.index('x_axis_joint')]
-                y = msg.position[msg.name.index('y_axis_joint')]
-                z = msg.position[msg.name.index('z_axis_lift_joint')]
+                # Find names in your URDF
+                idx_x = msg.name.index('x_axis_joint')
+                idx_y = msg.name.index('y_axis_joint')
+                idx_z = msg.name.index('z_axis_lift_joint')
+
+                x = msg.position[idx_x]
+                y = msg.position[idx_y]
+                z = msg.position[idx_z]
                 
                 command = f"X{x:.4f}Y{y:.4f}Z{z:.4f}\n"
                 self.ser.write(command.encode('utf-8'))
-            except (ValueError, IndexError):
-                pass
+                
+                # THIS FOR DEBUGGING:
+                print(f"Sent to Arduino: {command.strip()}", end='\r')
+
+            except (ValueError, IndexError) as e:
+                print(f"Waiting for joints... Error: {e}")
 def main(args=None):
     rclpy.init(args=args)
     node = SerialBridge()
